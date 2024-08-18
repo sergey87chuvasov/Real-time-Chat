7b4bcb612b20984d5d201ecad9b74b8222d3b0a1
Sergey87 <serge.techno@gmail.com>
2024-08-19 01:00:48 +0300

    work with login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,43 @@
 import './Login.css';
+import assets from '../../assets/assets';
 
 const Login = () => {
-  return <div className='login'>login page</div>;
+  return (
+    <div className='login'>
+      <img src={assets.logo_big} alt='logo pic' className='logo' />
+      <form className='login-form'>
+        <h2>Sign Up</h2>
+        <input
+          type='text'
+          placeholder='Username'
+          className='form-input'
+          required
+        />
+        <input
+          type='email'
+          placeholder='Email address'
+          className='form-input'
+          required
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          className='form-input'
+          required
+        />
+        <button type='submit'>Sign Up</button>
+        <div className='login-term'>
+          <input type='checkbox' />
+          <p>Agree to the terms of use & privacy policy</p>
+        </div>
+        <div className='login-forgot'>
+          <p className='login-toggle'>
+            Already have an account? <span>click here</span>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
