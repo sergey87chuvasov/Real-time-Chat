623191856ee991feec434d2eb2efcb70830747d0
Sergey87 <serge.techno@gmail.com>
2024-08-21 00:02:59 +0300

    work with chat page - 3 part

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -7,9 +7,40 @@ const ChatBox = () => {
       <div className='chat-user'>
         <img src={assets.profile_img} alt='img pic' />
         <p>
-          Richard Sanford <img src={assets.green_dot} alt='img pic' />
+          Richard Sanford{' '}
+          <img className='dot' src={assets.green_dot} alt='img pic' />
         </p>
         <img src={assets.help_icon} alt='help icon' className='help' />
+      </div>
+
+      <div className='chat-msg'>
+        <div className='s-msg'>
+          <p className='msg'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing...
+          </p>
+          <div>
+            <img src={assets.profile_img} alt='img pic' />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+        <div className='r-msg'>
+          <p className='msg'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing...
+          </p>
+          <div>
+            <img src={assets.profile_img} alt='img pic' />
+            <p>2:30 PM</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='chat-input'>
+        <input type='text' placeholder='Send a message' />
+        <input type='file' id='image' accept='image/png, image/jpeg' hidden />
+        <label htmlFor='image'>
+          <img src={assets.gallery_icon} alt='icon pic' />
+        </label>
+        <img src={assets.send_button} alt='icon pic' />
       </div>
     </div>
   );
