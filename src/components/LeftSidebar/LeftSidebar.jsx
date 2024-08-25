c62ac16e3c667e14dd373c5869b7d2691de90651
Sergey87 <serge.techno@gmail.com>
2024-08-26 01:12:55 +0300

    added connection user with another user in chat

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,24 +1,84 @@
 import './LeftSidebar.css';
 import assets from '../../assets/assets';
 import { useNavigate } from 'react-router-dom';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import {
+  arrayUnion,
+  collection,
+  doc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
 import { db } from '../../config/firebase';
+import { useContext, useState } from 'react';
+import { AppContext } from '../../context/AppContext';
+import { toast } from 'react-toastify';
 
 const LeftSidebar = () => {
   const navigate = useNavigate();
+  const { userData } = useContext(AppContext);
+  const [user, setUser] = useState(null);
+  const [showSearch, setShowSearch] = useState(false);
 
   const inputHandler = async (e) => {
     try {
       const input = e.target.value;
-      const userRef = collection(db, 'users');
-      const q = query(userRef, where('username', '==', input.toLowerCase()));
-      const querySnap = await getDocs(q);
 
-      if (!querySnap.empty) {
-        console.log(querySnap.docs[0].data());
+      if (input) {
+        setShowSearch(true);
+        const userRef = collection(db, 'users');
+        const q = query(userRef, where('username', '==', input.toLowerCase()));
+        const querySnap = await getDocs(q);
+
+        if (!querySnap.empty && querySnap.docs[0].data().id !== userData.id) {
+          setUser(querySnap.docs[0].data());
+        } else {
+          setUser(null);
+        }
+      } else {
+        setShowSearch(false);
       }
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  const addChat = async () => {
+    const messagesRef = collection(db, 'messages');
+    const chatsRef = collection(db, 'chats');
+
+    try {
+      const newMessageRef = doc(messagesRef);
+      await setDoc(newMessageRef, {
+        createAt: serverTimestamp(),
+        messages: [],
+      });
+
+      await updateDoc(doc(chatsRef, user.id), {
+        chatsData: arrayUnion({
+          messageId: newMessageRef.id,
+          lastMessage: '',
+          rId: userData.id,
+          updatedAt: Date.now(),
+          messageSeen: true,
+        }),
+      });
+
+      await updateDoc(doc(chatsRef, userData.id), {
+        chatsData: arrayUnion({
+          messageId: newMessageRef.id,
+          lastMessage: '',
+          rId: user.id,
+          updatedAt: Date.now(),
+          messageSeen: true,
+        }),
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
     }
   };
   return (
@@ -45,17 +105,24 @@ const LeftSidebar = () => {
         </div>
       </div>
       <div className='ls-list'>
-        {Array(12)
-          .fill('')
-          .map((item, index) => (
-            <div key={index} className='friends'>
-              <img src={assets.profile_img} alt='img pic' />
-              <div>
-                <p>Ricard Sanford</p>
-                <span>Hello, How are you?</span>
+        {showSearch && user ? (
+          <div onClick={addChat} className='friends add-user'>
+            <img src={user.avatar} alt='user ava' />
+            <p>{user.name}</p>
+          </div>
+        ) : (
+          Array(12)
+            .fill('')
+            .map((item, index) => (
+              <div key={index} className='friends'>
+                <img src={assets.profile_img} alt='img pic' />
+                <div>
+                  <p>Ricard Sanford</p>
+                  <span>Hello, How are you?</span>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+        )}
       </div>
     </div>
   );
